c4732187ce8d95392bab0bf4677ca2b42233ec41
Hanaya03 <ha4863519@gmail.com>
2024-12-16 15:01:43 -0800

    created source code file and folder. created variables for hardware

---
+++ source_code/interpreter.c
@@ -0,0 +1,25 @@
+int main(void) {
+    //initializing hardware
+    unsigned char memory[4096];
+    unsigned char delay_timer;
+    unsigned char sound_timer;
+    //registers
+    unsigned short I;
+    unsigned short pc;
+    unsigned char v0;
+    unsigned char v1;
+    unsigned char v2;
+    unsigned char v3;
+    unsigned char v4;
+    unsigned char v5;
+    unsigned char v6;
+    unsigned char v7;
+    unsigned char v8;
+    unsigned char v9;
+    unsigned char va;
+    unsigned char vb;
+    unsigned char vc;
+    unsigned char vd;
+    unsigned char ve;
+    unsigned char vf;
+}
