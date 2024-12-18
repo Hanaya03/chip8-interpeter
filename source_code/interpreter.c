1cc2a64f926f126e4ef36f247c5148dd117c534e
Hanaya03 <ha4863519@gmail.com>
2024-12-18 00:49:59 -0800

    created helper function 'write_to_memory' and allowed emulator to accept input via parameter

--- source_code/interpreter.c
+++ source_code/interpreter.c
@@ -1,4 +1,29 @@
-int main(void) {
+#include <stdio.h>
+#include <stdlib.h>
+
+void write_to_memory(unsigned char *mem, int offset, unsigned char *data){
+    printf("hello function\n");
+    for(int i = 0; i < sizeof(data); i++){
+        mem[offset + i] = data[i];
+    }
+    printf("%d\n", mem[offset]);
+}
+
+int main(int argc, char** argv) {
+    //getting input file
+    FILE *fileptr;
+    char *buffer;
+    long filelen;
+
+    fileptr = fopen(argv[1], "rb");
+    fseek(fileptr, 0, SEEK_END);
+    filelen = ftell(fileptr);
+    rewind(fileptr);
+
+    buffer = (char *)malloc(filelen * sizeof(char));
+    fread(buffer, filelen, 1, fileptr);
+    fclose(fileptr);
+
     //initializing hardware
     unsigned char memory[4096];
     unsigned char delay_timer;
@@ -7,4 +32,7 @@ int main(void) {
     unsigned short I;
     unsigned short pc;
     unsigned char v[16];
+    
+    printf("%d\n", memory);
+    write_to_memory(memory, 200, buffer);
 }
