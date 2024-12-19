d3c125a997546dc03d17f7c6b35dfa3ae7014c8b
Hanaya03 <ha4863519@gmail.com>
2024-12-19 14:44:42 -0800

    created helper functions to manipulate array like a stack.

--- source_code/interpreter.c
+++ source_code/interpreter.c
@@ -2,11 +2,32 @@
 #include <stdlib.h>
 
 void write_to_memory(unsigned char *mem, int offset, unsigned char *data){
-    printf("hello function\n");
-    for(int i = 0; i < sizeof(data); i++){
+    for(int i = 0; i < sizeof(data)/sizeof(data[0]); i++){
         mem[offset + i] = data[i];
     }
-    printf("%d\n", mem[offset]);
+}
+
+struct stack{
+    unsigned char elements[20];
+    int curr_elements;
+};
+
+typedef struct stack Stack;
+
+void push_to_stack(Stack *stackptr, unsigned char data){
+    stackptr->elements[stackptr->curr_elements] = data;
+    stackptr->curr_elements++;
+    printf("after push: size:%d\n",stackptr->curr_elements);
+}
+
+unsigned char pop_stack(Stack *stackptr){
+    stackptr->curr_elements--;
+    return stackptr->elements[stackptr->curr_elements];
+}
+
+unsigned char peak_stack(Stack *stackptr){
+    int tmp = stackptr->curr_elements - 1;
+    return stackptr->elements[tmp];
 }
 
 int main(int argc, char** argv) {
@@ -24,15 +45,40 @@ int main(int argc, char** argv) {
     fread(buffer, filelen, 1, fileptr);
     fclose(fileptr);
 
-    //initializing hardware
+    //initializing components
     unsigned char memory[4096];
     unsigned char delay_timer;
     unsigned char sound_timer;
+    Stack chip_stack;
+    chip_stack.curr_elements = 0;
     //registers
     unsigned short I;
     unsigned short pc;
     unsigned char v[16];
+    //font data
+    unsigned char font[80] = {0xF0, 0x90, 0x90, 0x90, 0xF0,   // 0
+                              0x20, 0x60, 0x20, 0x20, 0x70,   // 1
+                              0xF0, 0x10, 0xF0, 0x80, 0xF0,   // 2
+                              0xF0, 0x10, 0xF0, 0x10, 0xF0,   // 3
+                              0x90, 0x90, 0xF0, 0x10, 0x10,   // 4
+                              0xF0, 0x80, 0xF0, 0x10, 0xF0,   // 5
+                              0xF0, 0x80, 0xF0, 0x90, 0xF0,   // 6
+                              0xF0, 0x10, 0x20, 0x40, 0x40,   // 7
+                              0xF0, 0x90, 0xF0, 0x90, 0xF0,   // 8
+                              0xF0, 0x90, 0xF0, 0x10, 0xF0,   // 9
+                              0xF0, 0x90, 0xF0, 0x90, 0x90,   // A
+                              0xE0, 0x90, 0xE0, 0x90, 0xE0,   // B
+                              0xF0, 0x80, 0x80, 0x80, 0xF0,   // C
+                              0xE0, 0x90, 0x90, 0x90, 0xE0,   // D
+                              0xF0, 0x80, 0xF0, 0x80, 0xF0,   // E
+                              0xF0, 0x80, 0xF0, 0x80, 0x80};  // F
     
-    printf("%d\n", memory);
-    write_to_memory(memory, 200, buffer);
+    write_to_memory(memory, 80, font);
+    write_to_memory(memory, 512, buffer);
+
+    printf("before push: size:%d\n",chip_stack.curr_elements);
+    push_to_stack(&chip_stack, 0x03);
+    printf("peak: %x ", peak_stack(&chip_stack));
+    printf("pop: %x ", pop_stack(&chip_stack));
+    printf("size: %d\n", chip_stack.curr_elements);
 }
